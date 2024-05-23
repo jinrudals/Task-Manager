aa00915609d6e6fc3968831bfaef069da232ed74
Benjamin Jin <benjamin.jin@bos-semi.com>
2024-05-23 16:42:25 +0900

    Use logger

--- libs/server.py
+++ libs/server.py
@@ -7,6 +7,7 @@ import websockets
 from libs import Redis
 
 # Configure logging
+logger = logging.getLogger(__name__)
 clients = defaultdict(set)
 
 
@@ -16,13 +17,13 @@ class Server:
         self.port = port
         self.redis = Redis.RedisClient(redis_host, redis_port)
         self.MAXIMUM = maximum
-        logging.debug("Server initialized with host %s, port %s, redis_host %s, redis_port %s, maximum %d",
-                      host, port, redis_host, redis_port, maximum)
+        logger.debug("Server initialized with host %s, port %s, redis_host %s, redis_port %s, maximum %d",
+                     host, port, redis_host, redis_port, maximum)
 
     async def serve(self):
         await self.redis.connect()
         server = await websockets.serve(self.handle_client, self.host, self.port)
-        logging.info("WebSocket server started on %s:%s", self.host, self.port)
+        logger.info("WebSocket server started on %s:%s", self.host, self.port)
 
         task = asyncio.create_task(server.wait_closed())
         task2 = asyncio.create_task(self.launch())
@@ -31,35 +32,35 @@ class Server:
 
     async def handle_client(self, ws, path):
         clients[path].add(ws)
-        logging.info("Client connected: %s", path)
+        logger.info("Client connected: %s", path)
         try:
             async for message in ws:
                 try:
                     message = json.loads(message)
-                    logging.debug("Received message: %s", message)
+                    logger.debug("Received message: %s", message)
                     action = message.get('action')
 
                     if action == "add":
                         message.pop("action")
                         message["channel"] = path
                         await self.redis.enqueue(message)
-                        logging.info("Message enqueued: %s", message)
+                        logger.info("Message enqueued: %s", message)
 
                     elif action == "complete":
                         message["channel"] = path
                         await self.redis.complete(message)
                         self.MAXIMUM += 1
-                        logging.info("Message completed: %s", message)
+                        logger.info("Message completed: %s", message)
 
                     elif action == "flush":
                         await self.redis.flush()
-                        logging.info("Redis flushed")
+                        logger.info("Redis flushed")
                 except Exception as e:
-                    logging.error("Error processing message: %s",
-                                  e, exc_info=True)
+                    logger.error("Error processing message: %s",
+                                 e, exc_info=True)
         except websockets.ConnectionClosed:
             clients[path].remove(ws)
-            logging.info("Client disconnected: %s", path)
+            logger.info("Client disconnected: %s", path)
 
     async def launch(self):
         while True:
@@ -67,15 +68,15 @@ class Server:
                 item = await self.redis.launch()
                 if item:
                     channel = item.get('channel')
-                    logging.debug("Launching item: %s", item)
+                    logger.debug("Launching item: %s", item)
                     for client in clients[channel]:
                         await client.send(json.dumps(item))
                     self.MAXIMUM -= 1
-                    logging.info("Item launched and sent to clients: %s", item)
+                    logger.info("Item launched and sent to clients: %s", item)
             await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.info)
+    logger.basicConfig(level=logging.info)
     server = Server("localhost", 8023, "localhost", 6379, 5)
     asyncio.run(server.serve())
